b759d989f7ca0e8da30822d0f6343c80ebb43740
Paulo André Pimenta Aragão <andrearagao1003@outlook.com>
2023-07-03 05:17:35 -0300

    Manifest 3 - Now the auto scroll works fine on manifest 3. Also, some improvements on the plugin have been made

--- service_worker.js
+++ service_worker.js
@@ -1,19 +1,48 @@
-let scale = 0.5;
+// Interval ID and page height
+let intId = -1;
+let height = 0;
 
-chrome.browserAction.onClicked.addListener(tab => {
-    chrome.tabs.executeScript(
-        tab.id,
-        {code: `
-        let height = 0;
-        const scale = ${scale};
-        let intervalID = setInterval(function () {
-            height+=scale;
-            document.body.scrollTo({ top: height });
-            window.scrollTo({ top: height });
-            if(height >= document.body.scrollHeight){
-                clearInterval(intervalID);
-            }
-        }, scale);
-        `}
-    );
+
+// Perform auto scroll
+function autoScroll() {
+    // Check if the interval ID is undefined
+    if(this.intId == undefined){
+        console.log("intId undefined");
+        
+    }
+
+    // If there is a interval ID, clear it, undefine it and return
+    else{
+        clearInterval(intId);
+        intId = undefined;
+        return;
+    }
+
+    if(this.height == undefined){
+        height = 0;
+    }
+    
+    // Scroll page
+    const scale = 0.5;
+    let intervalID = setInterval(function () {
+        height+=scale;
+        document.body.scrollTo({ top: height });
+        window.scrollTo({ top: height });
+        if(height >= document.body.scrollHeight){
+            clearInterval(intervalID);
+        }
+    }, scale);
+
+    // Set global interval ID
+    intId = intervalID;
+    return intervalID;
+}
+
+
+// On plugin icon click
+chrome.action.onClicked.addListener(function(tab) {
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        function: autoScroll,
+    });
 });
